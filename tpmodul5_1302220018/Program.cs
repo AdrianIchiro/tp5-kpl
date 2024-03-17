465d7889e4a47f65952736b997b7efd79c855c59
AdrianIchiro <adrianfahren10@gmail.com>
2024-03-18 01:26:26 +0700

    menambahkan class DataGeneric

--- tpmodul5_1302220018/Program.cs
+++ tpmodul5_1302220018/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class DataGeneric<T>
+{
+    private T data;
+    public DataGeneric(T nim)
+    {
+        this.data = nim;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang di tampilkan " + this.data);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataGeneric<string> data = new DataGeneric<string>("1302220018");
+        data.PrintData();
+    }
+}
--- tpmodul5_1302220018/obj/Debug/net8.0/tpmodul5_1302220018.AssemblyInfo.cs
+++ tpmodul5_1302220018/obj/Debug/net8.0/tpmodul5_1302220018.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpmodul5_1302220018")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bace81fcaf4bf3543247bb6989996c2b1e3f6a71")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpmodul5_1302220018")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpmodul5_1302220018")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
